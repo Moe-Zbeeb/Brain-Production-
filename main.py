5d1217e8b734074720e582d85fd3d460790f7421
Moe-Zbeeb <mhmdzbyb829@gmail.com>
2024-09-20 00:53:44 +0300

    Refactor base route and add environment variable loading
    - Refactored the base route in the `routes/base.py` file to include a prefix of "/api/v1" and tags of ["Base"].
    - Added code in `main.py` to load environment variables from the `.env` file using `python-dotenv`.
    - Updated the `.env` file to include the `OPENAI_API_KEY` variable.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI 
-from routes.base import base 
+from fastapi import FastAPI   
+from dotenv import load_dotenv    
+load_dotenv(".env")  
+from routes import base 
+
 
 app = FastAPI() 
 app.include_router(base.base_router)  
--- routes/base.py
+++ routes/base.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI,APIRouter 
+import os 
 
-base_router = APIRouter() 
+base_router = APIRouter(
+prefix = "/api/v1" ,
+tags = ["Base"]   ,
+)
 
 @base_router.get("/") 
-def read_root(): 
+def read_root():    
+    app_name = os.getenv("APP_NAME")
     return {"message": "Hello api is working"}  
