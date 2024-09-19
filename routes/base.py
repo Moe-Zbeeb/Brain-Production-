eea4be5b1030cc6a5d3d2f21e085ba328a394c4f
Moe-Zbeeb <mhmdzbyb829@gmail.com>
2024-09-20 01:01:18 +0300

    Refactor base route and update environment variables

--- routes/base.py
+++ routes/base.py
@@ -8,5 +8,6 @@ tags = ["Base"]   ,
 
 @base_router.get("/") 
 def read_root():    
-    app_name = os.getenv("APP_NAME")
-    return {"message": "Hello api is working"}  
+    app_name = os.getenv("APP_NAME")  
+    app_version = os.getenv("APP_VERSION")
+    return {"message": "Hello api is working fine", "app_name": app_name, "app_version": app_version}  
